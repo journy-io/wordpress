bfd6c0e16554ddaa04530f2c64cf8e1f9542d10a
Hans Ott <hansott@hotmail.be>
2021-03-30 10:51:51 +0200

    📝 Bump version to 2.0.10

--- journy-io.php
+++ journy-io.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: journy.io
  * Plugin URI: https://www.journy.io/
- * Version: 2.0.9
+ * Version: 2.0.10
  * Author: journy.io
  * Description: Activates and tracks Wordpress events into journy.io
  * License: GPL2
