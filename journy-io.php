03ea6cfd0b997d418684b9ca8743b546062cc20f
Chato De Veirman <chato@deveirman.it>
2021-04-15 23:54:04 +0200

    Bump version

--- journy-io.php
+++ journy-io.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: journy.io
  * Plugin URI: https://www.journy.io/
- * Version: 2.0.14
+ * Version: 2.0.15
  * Author: journy.io
  * Authro URI: https://jtm.journy.io/?utm_source=wordpress&utm_medium=wordpress-list-view&utm_campaign=yall&jtm_event=wordpress-list-clicked
  * Description: Activates and tracks WordPress events into journy.io
